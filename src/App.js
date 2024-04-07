9b43b8524da73f13e02d42574998e08832e14807
Lilianada <lilibest30.lb@gmail.com>
2024-04-06 20:55:36 -0400

    product overview

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./page/Home";
 import Contact from "./page/Contact";
 import About from "./page/About";
 import MarketPlace from "./page/MarketPlace";
+import ProductOverview from "./components/ProductOverview";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/contact-us" element={<Contact />}></Route>
           <Route path="/about-us" element={<About />}></Route>
           <Route path="/marketplace" element={<MarketPlace />}></Route>
+          <Route path="/marketplace/product/overview/:id" element={<ProductOverview />}></Route>
         </Routes>
       </Router>
     </div>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -49,10 +49,6 @@ const products = [
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
           <div className="flex items-center justify-between space-x-4">
             <h2 className="text-lg font-medium text-gray-900">All Available Templates</h2>
-            {/* <Link to="#" className="whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500">
-              View all
-              <span aria-hidden="true"> &rarr;</span>
-            </Link> */}
           </div>
           <div className="mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4">
             {products.map((product) => (
@@ -67,7 +63,7 @@ const products = [
                 </div>
                 <div className="mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900">
                   <h3>
-                    <Link to="#">
+                    <Link to={`/marketplace/product/overview/${product.id}`}>
                       <span aria-hidden="true" className="absolute inset-0" />
                       {product.name}
                     </Link>

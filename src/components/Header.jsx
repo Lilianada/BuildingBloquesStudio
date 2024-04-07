c87425a05bbc16dfe753f51981768ddd576d0135
Lilianada <lilibest30.lb@gmail.com>
2024-04-06 20:30:15 -0400

    upgrade header and product list

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,42 +1,114 @@
-import React, { useState } from 'react';
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline';
-import { Link } from 'react-router-dom';
-import { Dialog } from '@headlessui/react';
+import React, { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { Link } from "react-router-dom";
+import { Dialog } from "@headlessui/react";
 
 const navigation = [
-  { name: 'ABout Us', href: '/about-us' },
-]
+  { name: "About Us", href: "/about-us" },
+  { name: "Marketplace", href: "/marketplace" },
+  { name: "Careers", href: "/careers" },
+];
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
-          <div className="flex lg:flex-1">
-            <Link to="/" className="-m-1.5 p-1.5 border-2 rounded-full border-indigo-500">
-              <span className="sr-only">BuildingBloques</span>
-              <h4 className='font-bold text-indigo-500 italic text-xl'>BB</h4>
+      <nav
+        className="flex items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <Link
+            to="/"
+            className="-m-1.5 p-1.5 border-2 rounded-full border-indigo-500"
+          >
+            <span className="sr-only">BuildingBloques</span>
+            <h4 className="font-bold text-indigo-500 italic text-xl">BB</h4>
+          </Link>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div className="hidden lg:flex lg:gap-x-12">
+          {navigation.map((item) => (
+            <Link
+              key={item.name}
+              to={item.href}
+              className="text-sm font-semibold leading-6 text-white"
+            >
+              {item.name}
             </Link>
-          </div>
-          {/* <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <Link key={item.name} to={item.href} className="text-sm font-semibold leading-6 text-white">
-                {item.name}
+          ))}
+        </div>
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <Link
+            to="/contact-us"
+            className="text-sm font-semibold leading-6 text-white"
+          >
+            Contact Us <span aria-hidden="true">&rarr;</span>
+          </Link>
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-50" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
+          <div className="flex items-center justify-between">
+            <div className="flex lg:flex-1">
+              <Link
+                to="/"
+                className="-m-1.5 p-1.5 border-2 rounded-full border-indigo-500"
+              >
+                <span className="sr-only">BuildingBloques</span>
+                <h4 className="font-bold text-indigo-500 italic text-xl">BB</h4>
               </Link>
-            ))}
-          </div> */}
-          <div className="flex flex-1 justify-end gap-4">
-            <Link to="/about-us" className="text-sm font-semibold leading-6 text-white">
-              About Us 
-              {/* <span aria-hidden="true">&rarr;</span> */}
-            </Link>
-            <Link to="/contact-us" className="text-sm font-semibold leading-6 text-white">
-              Contact Us <span aria-hidden="true">&rarr;</span>
-            </Link>
+            </div>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-400"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
           </div>
-        </nav>
-      </header>
-
-  )
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/25">
+              <div className="space-y-2 py-6">
+                {navigation.map((item) => (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                  >
+                    {item.name}
+                  </Link>
+                ))}
+              </div>
+              <div className="py-6">
+                <Link
+                  href="/contact-us"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                >
+                  Contact Us
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </header>
+  );
 }

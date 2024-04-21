86654544b2aee33ed96632c33cb65f3ae0541bdc
Lilianada <lilibest30.lb@gmail.com>
2024-04-21 10:31:59 -0400

    removed socials links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -143,12 +143,12 @@ const navigation = {
 
           <div className="mt-8 border-t border-white/10 pt-8 md:flex md:items-center md:justify-between">
             <div className="flex space-x-6 md:order-2">
-              {navigation.social.map((item) => (
+              {/* {navigation.social.map((item) => (
                 <a key={item.name} href={item.href} className="text-gray-500 hover:text-gray-400">
                   <span className="sr-only">{item.name}</span>
                   <item.icon className="h-6 w-6" aria-hidden="true" />
                 </a>
-              ))}
+              ))} */}
             </div>
             <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
               &copy; 2024 Building Bloquest Creative Studio, Ltd. All rights reserved.

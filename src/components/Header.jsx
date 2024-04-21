40a7dedb8699cc551338874fd070de75cc8b68d6
Lilianada <lilibest30.lb@gmail.com>
2024-04-21 13:47:50 -0400

    fixed sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
-import { Dialog } from "@headlessui/react";
 import logo from "../assets/logo.png";
-// import logo from '../assets/logo.svg';
+import Sidebar from "./Sidebar";
 
 const navigation = [
   { name: "About Us", href: "/about-us" },
-  // { name: "Marketplace", href: "/marketplace" },
+  { name: "Contact Us", href: "/contact-us" },
   // { name: "Careers", href: "/careers" },
 ];
 
@@ -15,6 +14,7 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
+    <>
     <header className="absolute inset-x-0 top-0 z-50">
       <nav
         className="flex items-center justify-between p-6 lg:px-8"
@@ -59,58 +59,12 @@ export default function Header() {
           </Link>
         </div>
       </nav>
-      <Dialog
-        as="div"
-        className="lg:hidden"
-        open={mobileMenuOpen}
-        onClose={setMobileMenuOpen}
-      >
-        <div className="fixed inset-0 z-50" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
-          <div className="flex items-center justify-between">
-            <div className="flex lg:flex-1">
-              <Link
-                to="/"
-                className="-m-1.5 p-1.5"
-              >
-                <span className="sr-only">BuildingBloques</span>
-            <img className="h-12 w-auto" src={logo} alt="Building bloques creative studio" />
-              </Link>
-            </div>
-            <button
-              type="button"
-              className="-m-2.5 rounded-md p-2.5 text-gray-400"
-              onClick={() => setMobileMenuOpen(false)}
-            >
-              <span className="sr-only">Close menu</span>
-              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-            </button>
-          </div>
-          <div className="mt-6 flow-root">
-            <div className="-my-6 divide-y divide-gray-500/25">
-              <div className="space-y-2 py-6">
-                {navigation.map((item) => (
-                  <Link
-                    key={item.name}
-                    href={item.href}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-              <div className="py-6">
-                <Link
-                  href="/contact-us"
-                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
-                >
-                  Contact Us
-                </Link>
-              </div>
-            </div>
-          </div>
-        </Dialog.Panel>
-      </Dialog>
     </header>
+      <Sidebar
+        sidebarOpen={mobileMenuOpen}
+        setSidebarOpen={setMobileMenuOpen}
+        navigation={navigation}
+        />
+    </>
   );
 }

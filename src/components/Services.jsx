b9c22cdaacb994f7335355b2da7c83b4c2dec194
Lilianada <lilibest30.lb@gmail.com>
2024-04-22 10:10:36 -0400

    services and team pages

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import React from "react";
+import { CheckCircleIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+
+const services = [
+  "Brand Identity Design",
+  "UI and UX Design",
+  "Web and mobile app development",
+  "CRM systems",
+  "Content Management System (CMS) setup and customization",
+  "Cloud infrastructure setup and management",
+];
 
 export default function Services() {
   return (
-    <div>
-      
+    <div className="relative isolate -z-10 mt-32 sm:mt-40">
+      <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
+          <img
+            className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
+            src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
+            alt=""
+          />
+          <div className="w-full flex-auto">
+            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+              Our Services
+            </h2>
+            <p className="mt-6 text-lg leading-8 text-gray-300">
+              Based on our product vision and goals, here's a breakdown of
+              potential services offered
+            </p>
+            <ul className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-white sm:grid-cols-2">
+              {services.map((item) => (
+                <li key={item} className="flex gap-x-3">
+                  <CheckCircleIcon
+                    className="h-7 w-5 flex-none"
+                    aria-hidden="true"
+                  />
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <div className="mt-10 flex">
+              <Link
+                to="/contact-us"
+                className="text-sm font-semibold leading-6 text-indigo-400"
+              >
+                For custom service <span aria-hidden="true">&rarr;</span>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        className="absolute inset-x-0 -top-16 -z-10 flex transform-gpu justify-center overflow-hidden blur-3xl"
+        aria-hidden="true"
+      >
+        <div
+          className="aspect-[1318/752] w-[82.375rem] flex-none bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25"
+          style={{
+            clipPath:
+              "polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)",
+          }}
+        />
+      </div>
     </div>
-  )
+  );
 }
--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,30 +1,17 @@
 import React from "react";
-import {
-  CheckCircleIcon,
-  HandRaisedIcon,
-  RocketLaunchIcon,
-  WrenchScrewdriverIcon,
-  UserGroupIcon,
-} from "@heroicons/react/20/solid";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { Link } from "react-router-dom";
 import StartupMission from "../components/StartupMission";
+import Services from "../components/Services";
+import Team from "../components/Team";
+import { Link } from "react-router-dom";
+import SpiderWeb from "../assets/logo.png"
 
 const stats = [
   { label: "Business was founded", value: "2022" },
   { label: "People on the team", value: "10+" },
   { label: "Number of Clients", value: "20+" },
   { label: "Number of Projects", value: "50+" },
-];
-
-const benefits = [
-  "Brand Identity Design",
-  "UI and UX Design",
-  "Web and mobile app development",
-  "CRM systems",
-  "Content Management System (CMS) setup and customization",
-  "Cloud infrastructure setup and management",
 ];
 
 export default function About() {
@@ -48,53 +35,41 @@ export default function About() {
           />
         </div>
 
-        {/* Header section */}
         <div className="px-6 pt-14 lg:px-8">
           <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
             <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+              {" "}
               Why Building Bloques?
             </h2>
             <p className="mt-6 text-lg leading-8 text-gray-300">
               Building Bloques was born from a bold vision to empower
               entrepreneurs and businesses to achieve success through
-              innovation. We started by bridging the gap between ideas and
-              execution in design and technology. Now, we've grown into a
-              comprehensive solution provider, offering a full spectrum of
-              services from business creation to cutting-edge technology
-              solutions.
+              innovation.
             </p>
           </div>
         </div>
 
-        <div className="mx-auto mt-20 max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto mt-16 max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-none">
-            {/* <div className="grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 lg:max-w-none lg:grid-cols-2">
+            <div className="grid max-w-xl grid-cols-1 gap-8 text-base leading-7 text-gray-300 lg:max-w-none lg:grid-cols-2">
               <div>
                 <p>
-                  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
-                  vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque
-                  erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
-                  semper sed amet vitae sed turpis id.
-                </p>
-                <p className="mt-8">
-                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie
-                  auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices
-                  hac adipiscing egestas.
+                  We started by bridging the gap between ideas and execution in
+                  design and technology. Now, we've grown into a comprehensive
+                  solution provider, offering a full spectrum of services from
+                  business creation to cutting-edge technology solutions.
                 </p>
               </div>
               <div>
                 <p>
-                  Erat pellentesque dictumst ligula porttitor risus eget et eget. Ultricies tellus felis id dignissim
-                  eget. Est augue maecenas risus nulla ultrices congue nunc tortor. Enim et nesciunt doloremque nesciunt
-                  voluptate.
-                </p>
-                <p className="mt-8">
-                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie
-                  auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices
-                  hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+                  We're a passionate team of creatives, strategists, and
+                  technologists united by a common goal: turning ideas into
+                  reality and propelling businesses forward. We foster a culture
+                  of curiosity and collaboration, where diverse perspectives
+                  come together to challenge the status quo.
                 </p>
               </div>
-            </div> */}
+            </div>
             <dl className="mt-16 grid grid-cols-1 gap-x-8 gap-y-12 sm:mt-20 sm:grid-cols-2 sm:gap-y-16 lg:mt-28 lg:grid-cols-4">
               {stats.map((stat, statIdx) => (
                 <div
@@ -112,24 +87,7 @@ export default function About() {
             </dl>
           </div>
         </div>
-        <div className="px-6 pt-14 lg:px-8">
-          <div className="mx-auto max-w-2xl pt-24 text-center sm:pt-40">
-            <h2 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-              Who We Are?
-            </h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              We're a passionate team of creatives, strategists, and
-              technologists united by a common goal: turning ideas into reality
-              and propelling businesses forward. We foster a culture of
-              curiosity and collaboration, where diverse perspectives come
-              together to challenge the status quo.
-            </p>
-          </div>
-        </div>
 
-        {/* Content section */}
-
-        {/* Image section */}
         <div className="mt-32 sm:mt-40 xl:mx-auto xl:max-w-7xl xl:px-8">
           <img
             src="https://images.unsplash.com/photo-1521737852567-6949f3f9f2b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2894&q=80"
@@ -138,67 +96,32 @@ export default function About() {
           />
         </div>
 
-        {/* Startup Mission section */}
-              <StartupMission />
+        <StartupMission />
 
-        {/* Team section */}
-        {/* <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-40 lg:px-8">
-          <div className="mx-auto max-w-2xl lg:mx-0">
-            <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">Our team</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-300">
-              Excepturi repudiandae alias ut. Totam aut facilis. Praesentium in neque vel omnis. Eos error odio. Qui
-              fugit voluptatibus eum culpa.
-            </p>
-          </div>
-          <ul
-            role="list"
-            className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-14 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 xl:grid-cols-4"
-          >
-            {team.map((person) => (
-              <li key={person.name}>
-                <img className="aspect-[14/13] w-full rounded-2xl object-cover" src={person.imageUrl} alt="" />
-                <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-white">{person.name}</h3>
-                <p className="text-base leading-7 text-gray-300">{person.role}</p>
-                <p className="text-sm leading-6 text-gray-500">{person.location}</p>
-              </li>
-            ))}
-          </ul>
-        </div> */}
+        <Services />
+        
+        <Team />
 
-        {/* Services section */}
         <div className="relative isolate -z-10 mt-32 sm:mt-40">
           <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="mx-auto flex max-w-2xl flex-col gap-16 bg-white/5 px-6 py-16 ring-1 ring-white/10 sm:rounded-3xl sm:p-8 lg:mx-0 lg:max-w-none lg:flex-row lg:items-center lg:py-20 xl:gap-x-20 xl:px-20">
-              <img
-                className="h-96 w-full flex-none rounded-2xl object-cover shadow-xl lg:aspect-square lg:h-auto lg:max-w-sm"
-                src="https://images.unsplash.com/photo-1519338381761-c7523edc1f46?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80"
-                alt=""
-              />
+              
               <div className="w-full flex-auto">
                 <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                  Our Services
+                  Become a Partner
                 </h2>
                 <p className="mt-6 text-lg leading-8 text-gray-300">
-                  Based on our product vision and goals, here's a breakdown of
-                  potential services offered
+                  We are always looking for partners who share our vision and
+                  values. <br/>If you are passionate about innovation and growth,
+                  let's work together to build a better future.
                 </p>
-                <ul className="mt-10 grid grid-cols-1 gap-x-8 gap-y-3 text-base leading-7 text-white sm:grid-cols-2">
-                  {benefits.map((benefit) => (
-                    <li key={benefit} className="flex gap-x-3">
-                      <CheckCircleIcon
-                        className="h-7 w-5 flex-none"
-                        aria-hidden="true"
-                      />
-                      {benefit}
-                    </li>
-                  ))}
-                </ul>
+                
                 <div className="mt-10 flex">
                   <Link
-                    to="/contact"
+                    to="/contact-us"
                     className="text-sm font-semibold leading-6 text-indigo-400"
                   >
-                    For custom service <span aria-hidden="true">&rarr;</span>
+                    Send us a message <span aria-hidden="true">&rarr;</span>
                   </Link>
                 </div>
               </div>
@@ -217,6 +140,7 @@ export default function About() {
             />
           </div>
         </div>
+        
       </main>
 
       <Footer />

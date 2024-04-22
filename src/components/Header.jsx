713cd35e330de9e6169ef7b40065087df1989ec7
Lilianada <lilibest30.lb@gmail.com>
2024-04-22 17:31:05 -0400

    update header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,11 @@
-import React, { useState } from "react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import Sidebar from "./Sidebar";
-
-const navigation = [
-  { name: "About Us", href: "/about-us" },
-  // { name: "Contact Us", href: "/contact-us" },
-  // { name: "Careers", href: "/careers" },
-];
-
-const mobileNavigation = [
-  { name: "About Us", href: "/about-us" },
-  { name: "Contact Us", href: "/contact-us" },
-  // { name: "Careers", href: "/careers" },
-];
 
 export default function Header() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+ 
 
   return (
-    <>
     <header className="absolute inset-x-0 top-0 z-50">
       <nav
         className="flex items-center justify-between p-6 lg:px-8"
@@ -36,25 +21,17 @@ export default function Header() {
           </Link>
         </div>
         <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400"
-            onClick={() => setMobileMenuOpen(true)}
+        <Link
+            to="/contact-us"
+            className="text-sm font-semibold leading-6 text-white"
           >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
+            Contact Us <span aria-hidden="true">&rarr;</span>
+          </Link>
         </div>
         <div className="hidden lg:flex lg:gap-x-12">
-          {navigation.map((item) => (
-            <Link
-              key={item.name}
-              to={item.href}
-              className="text-sm font-semibold leading-6 text-white"
-            >
-              {item.name}
-            </Link>
-          ))}
+            <h3 className="text-sm font-semibold leading-6 text-white">
+             Building Bloques
+            </h3>
         </div>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <Link
@@ -66,11 +43,5 @@ export default function Header() {
         </div>
       </nav>
     </header>
-      <Sidebar
-        sidebarOpen={mobileMenuOpen}
-        setSidebarOpen={setMobileMenuOpen}
-        navigation={mobileNavigation}
-        />
-    </>
   );
 }
